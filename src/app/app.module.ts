a5df5e330ae948e3c3e94528b22934689e205b14
Cogentx <cogentx@live.com>
2019-11-21 08:05:51 -0330

    added cordova plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { AppRoutingModule } from './app-routing.module';
     IonicStorageModule.forRoot(),
     AppRoutingModule
   ],
-
   providers: [
     SocialSharing,
     LocalNotifications,
